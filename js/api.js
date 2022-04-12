ec14c44795d2f87e5b3e2617904eb70fdd881606
Isaac Hollow Horn Bear <isaachollowhorn@gmail.com>
2022-04-11 17:11:49 -0700

    cocktails api

---
+++ js/api.js
@@ -0,0 +1,21 @@
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+    let drink = document.querySelector('input').value
+
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+            console.log(data.drinks[0])
+            let choice = Math.floor(Math.random() * data.drinks.length)
+                {
+                    document.querySelector('h2').innerText = data.drinks[choice].strDrink
+                    document.querySelector('img').src = data.drinks[choice].strDrinkThumb
+                    document.querySelector('h3').innerText = data.drinks[choice].strInstructions
+                } 
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
